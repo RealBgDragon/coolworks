fc62b034699eef18e2849b0ebd5aa98e67acba5c
RealBgDragon <128041836+RealBgDragon@users.noreply.github.com>
2024-08-16 11:29:00 +0300

    debugging

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 use app\controllers\ModelController;
 use app\controllers\ProjectsController;
 use app\controllers\SiteController;
